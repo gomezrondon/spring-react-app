daaba0f1dd49d4933d4fb5e5fd9a4cca3d50e585
gomezrondon <gomezrondon@gmail.com>
2020-10-17 09:02:48 -0600

    Cleaning front-end, adding mock components.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FirstComponet extends React.Component {
+    render() {
+        return (
+            <div className="firstComponent">
+                First component
+            </div>
+        );
+    }
 }
+
+const SecondComponet = () =>{
+
+    return (
+        <div className="secondComponetn">
+            Second component
+        </div>
+    );
+
+}
+
+
+class App extends Component{
+    render() {
+        return (
+            <div className="App">
+                <FirstComponet/>
+                <SecondComponet/>
+            </div>
+        );
+    }
+}
+
 
 export default App;
